56f9197c198c729b522ea944ab748b942eafc5c3
Jason Cardinal <jason.cardinal@timiosdevelopment.com>
2020-11-02 19:17:59 -0600

    use termion

--- src/main.rs
+++ src/main.rs
@@ -2,65 +2,48 @@
 /// much like fzf but with the specific purpose to navigate
 /// the filesystem
 use std::error::Error;
+use std::io::{stdout, Read, Write};
 use std::path::Path;
+use std::path::PathBuf;
+use std::process::Stdio;
+use std::time::Duration;
 
 use pancurses::{endwin, initscr, noecho, Input, Window};
+use tokio::io::Lines;
+use tokio::io::{AsyncBufReadExt, BufReader};
+use tokio::process::ChildStdout;
+use tokio::process::Command;
 
-fn display_prompt(window: &Window, dir: &Path, input: &str) -> Result<(), Box<dyn Error>> {
-    let absolute_path = std::fs::canonicalize(&dir)?;
-
-    let prompt = format!("{} {}", absolute_path.to_string_lossy(), input);
-    window.mvprintw(0, 0, &prompt);
-
-    Ok(())
-}
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
 
 #[tokio::main]
-pub async fn main() -> Result<(), Box<dyn Error>> {
-    // get the current directory
-    let mut dir = Path::new(".");
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let stdout = stdout();
+    let mut stdout = stdout.lock().into_raw_mode().unwrap();
+    let mut stdin = termion::async_stdin().bytes();
 
-    let mut input = String::new();
-
-    // setup the ncurses screen
-    let window = initscr();
-    display_prompt(&window, dir, "")?;
-    window.refresh();
-    window.keypad(true);
-    noecho();
+    write!(
+        stdout,
+        "{}{}",
+        termion::clear::All,
+        termion::cursor::Goto(1, 1)
+    )
+    .unwrap();
 
     loop {
-        match window.getch() {
-            Some(Input::Character(c)) => {
-                input.push(c);
-                eprintln!("{:?}", input);
-            }
-            Some(Input::KeyDC) => break,
-            Some(Input::KeyBackspace) => {
-                if input.len() < 1 {
-                    input.clear();
-                } else {
-                    input = input.chars().take(input.len() - 1).collect::<String>();
-                }
+        let b = stdin.next();
 
-                eprintln!("{:?}", input);
-                window.clear();
+        if let Some(ch) = b {
+            write!(stdout, "\r{:?}    <- This demonstrates the async read input char. Between each update a 100 ms. is waited, simply to demonstrate the async fashion. \n\r", ch).unwrap();
+            if let Ok(b'q') = ch {
+                break;
             }
-            Some(input) => {
-                eprintln!("{:?}", input);
-            }
-            None => (),
         }
 
-        display_prompt(&window, dir, &input)?;
-        window.refresh();
-
-        // display the current promp
-        // display_prompt(&window, dir, &input)?;
-        // window.refresh();
+        write!(stdout, "{}", termion::cursor::Goto(1, 1)).unwrap();
+        stdout.flush().unwrap();
     }
-
-    endwin();
 
     Ok(())
 }
